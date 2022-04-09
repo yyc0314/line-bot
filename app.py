d9fa9c1e4f45eded9b2e99097ad4d64f354543d0
yyc0314 <97279109+yyc0314@users.noreply.github.com>
2022-04-09 19:33:43 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 21:16:35 2021
+
+@author: Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+
+Line Bot聊天機器人
+第一章 Line Bot申請與串接
+Line Bot機器人串接與測試
+"""
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('zVkUr0O4yEQo3FyR7rCjOZkH/wHWG8e4tnRW6l3iU6kJ/fJxXQAveMeFYBugrIC4jz1dsvRRYIAa7c2LvTcOL9fbLwodHQl8xSKrj8gvOZ6U0rf1Db0TLEPlmFeZhC6PhElulVEWcbTbrzkg3Sj54AdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('ac5b69f52f6a568c4cb6b8c2db7a834b')
+
+line_bot_api.push_message('U2b030a5933594945dc1ca06a78690cd6', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        confirm_template_message = TemplateSendMessage(
+            alt_text='舞蹈00',
+            template=ConfirmTemplate(
+                text='大學想要選什麼科系?',
+                actions=[
+                    MessageAction(
+                        label='a',
+                        text='a：舞蹈藝術相關科系'
+                    ),
+                    MessageAction(
+                        label='b',
+                        text='b：非藝術相關科系(例：醫學、電機)'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, confirm_template_message)
+
+def handle_message(event):
+    message = text=event.message.text
+  #  if re.match('告訴我秘密',message):
+        confirm_template_message = TemplateSendMessage(
+            alt_text='舞蹈00',
+            template=ConfirmTemplate(
+                text='大學想要選什麼科系?',
+                actions=[
+                    MessageAction(
+                        label='a',
+                        text='a：舞蹈藝術相關科系'
+                    ),
+                    MessageAction(
+                        label='b',
+                        text='b：非藝術相關科系(例：醫學、電機)'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, confirm_template_message)
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
