3fd44af491a831794e19535f3b39826b9549ab7a
yyc0314 <97279109+yyc0314@users.noreply.github.com>
2022-04-09 23:06:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -51,48 +51,9 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        confirm_template_message = TemplateSendMessage(
-            alt_text='舞蹈00',
-            template=ConfirmTemplate(
-                text='大學想要選什麼科系?',
-                actions=[
-                    MessageAction(
-                        label='a',
-                        text='a：舞蹈藝術相關科系'
-                    ),
-                    MessageAction(
-                        label='b',
-                        text='b：非藝術相關科系(例：醫學、電機)'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, confirm_template_message)
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
-#def handle_message(event):
-#    message = text=event.message.text
-  #  if re.match('告訴我秘密',message):
-#        line_bot_api.reply_message(event.reply_token, "（就讀舞蹈藝術相關科系後，學習了大量舞蹈的專業知識，也陸陸續續參加了很多表演）\n（大學生活的美好時光過得飛快......）\n\n友：欸欸啊啊啊你看！我之前去徵選的舞團上了！！比賽也過了！可是我英文很爛欸怎麼辦啦，要去國外很恐怖。\n你：別擔心啦，你能力那麼好，一定可以的！")
-#        line_bot_api.reply_message(event.reply_token,"（大學畢業前，同學一些從小學舞、長得好看、底子又好的去了國外的專業舞團）\n（那......接下來畢業後我要去哪呢？啊......想繼續跳舞但是我真的有能力讓自己在舞者這塊發光發熱嗎？）")
-#        confirm_template_message = TemplateSendMessage(
-#            alt_text='舞蹈1a',
-#            template=ConfirmTemplate(
-#                text='大學想要選什麼科系?',
-#                actions=[
-#                    MessageAction(
-#                        label='a',
-#                        text='a：從事專業舞者'
-#                    ),
-#                    MessageAction(
-#                        label='b',
-#                        text='b：轉換跑道，成為一位上班時間固定的上班族'
-#                    )
-#                ]
-#            )
-#        )
-#        line_bot_api.reply_message(event.reply_token, confirm_template_message)
 #主程式
 import os
 if __name__ == "__main__":
